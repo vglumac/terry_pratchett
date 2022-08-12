9baf0758565d2ad1127f7e38965a4b51b04a226b
vglumac <100020960+vglumac@users.noreply.github.com>
2022-08-12 22:21:14 +0200

    Add files via upload

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,138 +1,89 @@
-/* <div class="page-blur"></div>
-<div class="modal">
-    <div class="modal__container">
-        <img src="images/eric.jpg" alt="eric">
-        <h3>Eric</h3>
-        <p>Neki tekst ovde ide o knjizi i ima nekoliko redova, opisuje radnju knjige.</p>
-        <a href="#">Back</a>
-        <a href="#">More info</a>
-    </div>
-</div> */
+let openedBookModalIndex = null;
 
-function onClick() {
+const bookTitles = document.querySelectorAll('div.book');
 
-    const bookTitles = document.querySelectorAll('div.book');
+const booksByTitle = {};
+booksInfo.forEach(bookInfo => {
+    booksByTitle[bookInfo.title] = bookInfo;
+});
 
-    const booksByTitle = {};
-    booksInfo.forEach(bookInfo => {
-        booksByTitle[bookInfo.title] = bookInfo;
+
+bookTitles.forEach((bookTitle, index) => {
+    bookTitle.addEventListener('click', (e) => {
+        openedBookModalIndex = index;
+        openModal(openedBookModalIndex);
     });
+});
 
-    bookTitles.forEach((bookTitle, index) => {
-        bookTitle.addEventListener('click', (e) => {
-            const foundBook = booksByTitle[e.target.innerText];
-            createModal(foundBook, index, bookTitles, booksByTitle);
-
-        });
-    });
+function closeModal(modal) {
+    modal.remove();
+    window.removeEventListener('keydown', handleKeyDown);
 }
 
-function createModal(foundBook, index, bookTitles, booksByTitle) {
-    // add modal to DOM and set event handlers for closing (with click on backdrop, 'ESC' and 'exit' button)
-    let modal = document.createElement('div');
-    modal.classList.add('modal');
-    modal.innerHTML = `<div class="modal__container">
-                            <img src="${foundBook.cover}" alt="${foundBook.title}">
-                            <h3>${foundBook.title}</h3>
-                            <p>${foundBook.short}</p>
-                            <button id="exit-button">X</button>
-                            <button id="button-modal-back">PREVIOUS</button>
-                            <button id="button-modal-moreInfo">NEXT</button>
-                        </div>`;
-    switch (index) {
-        case 0: modal.querySelector('#button-modal-back').setAttribute('disabled', '');
-            break;
-        case (bookTitles.length - 1): modal.querySelector('#button-modal-moreInfo').setAttribute('disabled', '');
-            break;
+function handleKeyDown(event, modal) {
+    if (event.key === 'Escape') {
+        closeModal(modal);
+    }
+    if (event.key === 'ArrowRight') {
+        openModal(openedBookModalIndex + 1);
+    }
+    if (event.key === 'ArrowLeft') {
+        openModal(openedBookModalIndex - 1);
+    }
+};
+
+function getModal(title, short, cover) {
+    let modal = document.querySelector('.modal');
+    if (!modal) {
+        modal = document.createElement('div');
+        modal.classList.add('modal');
+        modal.innerHTML = `<div class="modal__container">
+                        <img alt="${title}" src="${cover}">
+                        <h3>${title}</h3>
+                        <p>${short}</p>
+                        <button id="exit-button">X</button>
+                        <button id="button-modal-back">PREVIOUS</button>
+                        <button id="button-modal-moreInfo">NEXT</button>
+                    </div>`;
     }
 
-    window.addEventListener('keydown', (event) => {
-        if (event.key === 'Escape') {
-            modal.remove();
-            window.removeEventListener('keydown', keyDownHandler);
-        }
-    });
+    window.addEventListener('keydown', (e) => handleKeyDown(e, modal));
+    modal.addEventListener('click', () => closeModal(modal));
+    modal.querySelector('.modal__container').addEventListener('click', (event) => event.stopPropagation());
+    modal.querySelector('#exit-button').addEventListener('click', () => closeModal(modal));
+    modal.querySelector('#button-modal-back').addEventListener('click', () => openModal(openedBookModalIndex - 1));
+    modal.querySelector('#button-modal-moreInfo').addEventListener('click', () => openModal(openedBookModalIndex + 1));
 
-    modal.addEventListener('click', () => {
-        modal.remove();
-        window.removeEventListener('keydown', keyDownHandler);
-    });
 
-    modal.querySelector('.modal__container').addEventListener('click', e => {
-        e.stopPropagation();
-    });
-
-    modal.querySelector('#exit-button').addEventListener('click', () => {
-        modal.remove();
-        window.removeEventListener('keydown', keyDownHandler);
-    });
+    // set event handlers for closing (with click on backdrop, 'ESC' and 'exit' button)
+    // set event handlers for next and previous same as for "arrowLeft" and "arrowRight"
 
     document.querySelector('body').appendChild(modal);
 
-    openModal(index, bookTitles, booksByTitle);
-
+    return modal;
 }
 
-function openModal(index, bookTitles, booksByTitle) {
-    // adds content to existing modal in DOM, event listeners etc.
-    let buttonNext = document.querySelector('#button-modal-moreInfo');
-    let buttonPrev = document.querySelector('#button-modal-back');
-
-    buttonNext.addEventListener('click', () => {
-        index++;
-        changeModal(index, bookTitles, booksByTitle, buttonNext, buttonPrev);
-        if (buttonPrev.getAttribute('disabled') === '') {
-            buttonPrev.removeAttribute('disabled');
-        }
-        if (index === bookTitles.length - 1) {
-            buttonNext.setAttribute('disabled', '');
-        }
-    });
-
-    buttonPrev.addEventListener('click', () => {
-        index--;
-        changeModal(index, bookTitles, booksByTitle, buttonNext, buttonPrev);
-        if (buttonNext.getAttribute('disabled') === '') {
-            buttonNext.removeAttribute('disabled');
-        }
-        if (index === 0) {
-            buttonPrev.setAttribute('disabled', '');
-        }
-    });
-
-    window.addEventListener('keydown', keyDownHandler);
-    function keyDownHandler(e) {
-        if (e.key === 'ArrowRight') {
-            index++;
-            changeModal(index, bookTitles, booksByTitle, buttonNext, buttonPrev);
-            if (buttonPrev.getAttribute('disabled') === '') {
-                buttonPrev.removeAttribute('disabled');
-            }
-        }
-        else if (e.key === 'ArrowLeft') {
-            index--;
-            changeModal(index, bookTitles, booksByTitle, buttonNext, buttonPrev);
-            if (buttonNext.getAttribute('disabled') === '') {
-                buttonNext.removeAttribute('disabled');
-            }
-            if (index === 0) {
-                buttonPrev.setAttribute('disabled', '');
-            }
-        }
+function openModal(nekiIndex) {
+    if (nekiIndex < 0 || nekiIndex >= bookTitles.length) {
+        return;
     }
+    const bookElement = bookTitles[nekiIndex];
+    const foundBook = booksByTitle[bookElement.innerText];
+
+    if (!foundBook) {
+        return;
+    }
+
+    const modal = getModal(foundBook.title, foundBook.short, foundBook.cover);
+    if (nekiIndex == 0) {
+        document.querySelector('#button-modal-back').setAttribute('disabled', '');
+
+    } else if (nekiIndex == bookTitles.length - 1) {
+        document.querySelector('#button-modal-moreInfo').setAttribute('disabled', '');
+    }
+
+
+    // fill modal content
+    // set disabled attribute on buttons (if needed)
 }
 
-
-function changeModal(index, bookTitles, booksByTitle, buttonNext, buttonPrev) {
-    let title = document.querySelector('.modal__container h3');
-    let img = document.querySelector('.modal__container img');
-    let short = document.querySelector('.modal__container p');
-    let newBook = {};
-    newBook = booksByTitle[bookTitles[index].innerText];
-    title.innerText = newBook.title;
-    img.src = newBook.cover;
-    img.alt = newBook.title;
-    short.innerText = newBook.short;
-}
-
-onClick();
